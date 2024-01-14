07ef8818a7b873591df5897c1ea44c4327961e5f
Luiz Henrique <luizboaventura24@gmail.com>
2024-01-14 14:09:07 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ tailwind.config = {
         'darkoil-900': '#101012',
         'darkoil-800': '#141416',
         'darkoil-700': '#161618',
-        'bluerize': '#1776e3'
+        'bluerize': '#1776e3',
+        'secondary-color': '#202024',
+        'primary-text': '#d8d8d8',
       },
     }
   }
